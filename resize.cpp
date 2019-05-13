457c4602a071f3f5fa890ebce0911c534b7804e6
Vicent Dolz <vdm97@hotmail.com>
2019-05-13 16:08:28 +0200

    Resize function

--- resize.cpp
+++ resize.cpp
@@ -2,16 +2,32 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <ctime>
-#include <iostream>
+#include <sstream>
 
 #define CVUI_IMPLEMENTATION
 #include "cvui-2.7.0/cvui.h"
 
-#define WINDOW_NAME	"Trackbar and columns"
+#define WINDOW_NAME	"Resize Image"
 using namespace cv;
 using namespace std;
 
+int timeVar = 50;
 
+void CheckSave(int* isSaved, Mat frame, Mat dst)
+{
+	if (*isSaved)
+	{
+		timeVar = timeVar -1;
+		cvui::printf(frame,100, 186, 0.5, 0x00ff00,"Image saved");
+		if(timeVar == 0) *isSaved = 0;
+	}
+	if (cvui::button("&Save"))
+	{
+		imwrite("Images/resize.jpg",dst);
+		*isSaved = 1;
+		timeVar = 50;
+	}
+}
 
 int main(int argc, const char *argv[])
 {
@@ -20,9 +36,12 @@ int main(int argc, const char *argv[])
   Mat src, dst;
   src = imread("Images/van_gogh.jpg", IMREAD_COLOR);
 
-	double rValue = 1;
+	double vValue = 1;
+	double hValue = 1;
+	bool keepProportion = true;
+	int vSize, hSize;
 	int isSaved = 0;
-
+	std::ostringstream size;
   if(!src.data)
   {
     cout << "Empty data" << endl;
@@ -40,21 +59,40 @@ int main(int argc, const char *argv[])
 
 		cvui::beginColumn(frame, 20, 20, -1, -1, 6);
 
+		cvui::checkbox(frame, 25, 10, "Keep Proportion", &keepProportion);
+		cvui::space(5);
 
-		cvui::text("Resize rate");
-		cvui::trackbar(width, &rValue, 0.2, 3., 1, "%.3Lf", cvui::TRACKBAR_DISCRETE, 0.001);
+		cvui::text("Vertical");
+		cvui::trackbar(width, &vValue, 0.2, 3., 1, "%.3Lf", cvui::TRACKBAR_DISCRETE, 0.001);
 
+		cvui::text("Horizontal");
+		cvui::trackbar(width, &hValue, 0.2, 3., 1, "%.3Lf", cvui::TRACKBAR_DISCRETE, 0.001);
 
-    resize(src, dst, Size(), rValue, rValue, INTER_CUBIC);
+		if(keepProportion){
+			resize(src, dst, Size(), vValue, vValue, INTER_CUBIC);
+			hSize = dst.cols;
+			vSize = dst.rows;
+			// cout <<  "H: " << hSize << " V: "<< vSize << endl	;
+			size << "H: " << hSize << " V: "<< vSize;
+
+		}
+		else{
+			resize(src, dst, Size(), hValue, vValue, INTER_CUBIC);
+			hSize = dst.cols;
+			vSize = dst.rows;
+			// cout << "H: " << hSize << " V: "<< vSize << endl;
+			size << "H: " << hSize << " V: "<< vSize;
+
+		}
+
     imshow("image",dst);
 
+		cvui::text(size.str());
+		size.clear();
+		size.str("");
 
-		if (isSaved) cvui::printf(frame,100, 95, 0.5, 0x00ff00,"Image saved");
-		if(cvui::button("&Save"))
-		{
-		 	imwrite("Images/resize.jpg",dst);
-		 	isSaved = 1;
-	 	}
+		CheckSave(&isSaved, frame, dst);
+
 		if (cvui::button("&Quit ")) {
 			break;
 		}
